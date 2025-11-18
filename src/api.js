80d024e3f4450cb29f7f73cc99926bcafe00abea
Chhalma Chhaya <chhaya.mist@gmail.com>
2025-11-18 22:18:01 +0000

    Add City Component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b6f2b0318fmshc180ec8330e6b6cp16d495jsn032b639429d8',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
